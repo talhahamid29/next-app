cd1b7dc62ba2802b4fe9a48b362eb1972c8af014
talhahamid92 <talha.syed@oxcytech.com>
2024-02-19 15:47:30 +0530

    initial commit

--- pages/api/displayproduct.ts
+++ pages/api/displayproduct.ts
@@ -1,16 +1,16 @@
-import { PrismaClient } from '@prisma/client';
-
-// Create a new instance of PrismaClient
-const prisma = new PrismaClient();
+import { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '@/lib/prisma';
 
 // API route handler
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     // Use Prisma context here
     const products = await prisma.product2.findMany();
+
+    // Return the products as a JSON response
     res.status(200).json(products);
-  } finally {
-    // Close the Prisma client to prevent leaks
-    await prisma.$disconnect();
+  } catch (error) {
+    // If an error occurs, send an error response
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 }
