20e468e5f796a1f66be73ea47713c25aeeb7c2d2
talhahamid92 <talha.syed@oxcytech.com>
2024-02-19 15:30:21 +0530

    initial commit

--- pages/api/addproduct.ts
+++ pages/api/addproduct.ts
@@ -1,29 +1,34 @@
 import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
 
-export default async function handler(req: NextRequest, res:NextResponse) {
+export default async function handler(req: NextRequest, res: NextResponse) {
   try {
-      //  const res = await req.json();
-    // // Process the data and add the product
-    // ...
+    // Ensure that the content type is application/json
+    if (req.headers.get('content-type')?.includes('application/json')) {
+      // Parse the JSON body
+      const body = await req.json();
 
-    if (req.body && req.body.productCategory) {
-    console.log('request object is', req.body.productCategory);
+      if (body && body.productCategory) {
+        console.log('request object is', body.productCategory);
         // Continue with your code
-    } else {
+      } else {
         console.log('Request body or productCategory is null or undefined.');
-    }
-    const result = await prisma.product2.create({
-      data : {
-        productCategory : req.body.productCategory,
-        productName: req.body.productName,
-        serialNumber: req.body.serialNumber,
-        price:  req.body.price
       }
-    })
 
-    res.status(200).json(result);
+      const result = await prisma.product2.create({
+        data: {
+          productCategory: body.productCategory,
+          productName: body.productName,
+          serialNumber: body.serialNumber,
+          price: body.price
+        }
+      });
 
+      res.status(200).json(result);
+    } else {
+      // If content type is not JSON, send an error response
+      throw new Error('Content type is not application/json');
+    }
   } catch (error) {
     console.error('Error during product addition:', error);
     return NextResponse.json({ success: false, error: 'Failed to add product' }, { status: 500 });
