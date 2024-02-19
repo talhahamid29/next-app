6d505ba2e07e6a9ae4d22ecdb08b883e4f1f875a
talhahamid92 <talha.syed@oxcytech.com>
2024-02-19 15:53:48 +0530

    initial commit

--- pages/api/updateproduct/[id].ts
+++ pages/api/updateproduct/[id].ts
@@ -1,29 +1,25 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextApiRequest, NextApiResponse } from 'next'; // Import the correct request type
 import prisma from '@/lib/prisma';
 
-export default async function handler(req: NextRequest, res:NextResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-      //  const res = await req.json();
-    // // Process the data and add the product
-    // ...
+    console.log('request object id is', req.query.id);
+    console.log('request object productCategory is', req.body.productCategoryModal);
 
-    console.log('request object id is' , req.query.id)
-    console.log('request object productCategory is' , req.body.productCategoryModal)
-
-    const result =  await prisma.product2.update({
-        where : {id : parseInt(req.query.id)},
-        data : {
-            productCategory : req.body.productCategoryModal,
-            productName: req.body.productNameModal,
-            serialNumber: req.body.serialNumberModal,
-            price:  req.body.priceModal
-          }
-    })
+    const result = await prisma.product2.update({
+      where: { id: parseInt(req.query.id as string) },
+      data: {
+        productCategory: req.body.productCategoryModal,
+        productName: req.body.productNameModal,
+        serialNumber: req.body.serialNumberModal,
+        price: req.body.priceModal
+      }
+    });
 
     res.status(200).json(result);
 
   } catch (error) {
     console.error('Error during product updation:', error);
-    return NextResponse.json({ success: false, error: 'Failed to update product' }, { status: 500 });
+    res.status(500).json({ success: false, error: 'Failed to update product' });
   }
 }
