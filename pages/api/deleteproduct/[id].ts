47d89e6ae8a5e81829b0894c660c233bdd78b267
talhahamid92 <talha.syed@oxcytech.com>
2024-02-19 15:44:13 +0530

    initial commit

--- pages/api/deleteproduct/[id].ts
+++ pages/api/deleteproduct/[id].ts
@@ -1,22 +1,17 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '@/lib/prisma';
 
-export default async function handler(req: NextRequest, res:NextResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-      //  const res = await req.json();
-    // // Process the data and add the product
-    // ...
+    console.log('request object is', req.query.id);
 
-    console.log('request object is' , req.query.id)
-
-    const result =  await prisma.product2.delete({
-        where : {id : parseInt(req.query.id)}
-    })
+    const result = await prisma.product2.delete({
+      where: { id: parseInt(req.query.id as string) }
+    });
 
     res.status(200).json(result);
-
   } catch (error) {
-    console.error('Error during product addition:', error);
-    return NextResponse.json({ success: false, error: 'Failed to add product' }, { status: 500 });
+    console.error('Error during product deletion:', error);
+    res.status(500).json({ success: false, error: 'Failed to delete product' });
   }
 }
